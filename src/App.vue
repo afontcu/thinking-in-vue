65a5e06b6860713f66da956029dafa241e79cd20
Adrià Fontcuberta <adria@calidae.com>
2018-03-06 11:36:39 +0100

    Initial commit 🚀

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="c-app">
+    <Title fancy>Product List</Title>
+    <FilterableProductTable :products="products" />
+  </div>
+</template>
+
+<script>
+import FilterableProductTable from './components/FilterableProductTable'
+import Title from '@/components/Title'
+
+
+export default {
+  name: 'App',
+  components: {
+    FilterableProductTable,
+    Title
+  },
+  data () {
+    return {
+      products
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  background-color: #eee;
+}
+body {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+  margin: 0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.c-app {
+  margin-top: 10vh;
+}
+</style>
